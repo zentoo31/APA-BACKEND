25a955447f2ceea2a367796f77c9f8f6ec4fc0a1
Diego Pineda <zentoo31@gmail.com>
2025-05-23 16:28:45 -0500

    add variables for frontend and port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.enableCors({
-    origin: 'http://localhost:3004', // Reemplaza con la URL de tu frontend
+    origin: configService.get('FRONTEND_URL') || 'http://localhost:3004',
     credentials: true,
   });
-  await app.listen(3000);
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
